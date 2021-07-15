12678dfd162f07bee4f1b39df292276284d93025
erictsai1224tw <48709873+erictsai1224tw@users.noreply.github.com>
2021-07-15 13:06:43 +0800

    Add files via upload

---
+++ B_ICC2017_preliminary_grad_cell-based/DT.v
@@ -0,0 +1,113 @@
+module DT(
+           input 	clk,
+           input	rstn,
+           output	done,
+           output	reg sti_rd,
+           output 	reg [9:0] sti_addr,
+           input	[15:0] sti_di,
+           output	reg res_wr,
+           output	reg res_rd,
+           output 	reg [13:0] res_addr,
+           output 	reg [7:0] res_do,
+           input	[7:0] res_di,
+	   output forward_pass_done,
+	output init_finish
+       );
+
+wire initialize_done, fp_done, bp_done;
+
+parameter init_p=0, fp_p=1, bp_p=2, done_p=3;
+reg [1:0]state, nstate;
+
+assign forward_pass_done=(fp_done);
+assign init_finish=initialize_done;
+
+always @(*) 
+begin
+    case (state)
+        init_p: nstate=(initialize_done)?fp_p:init_p;
+        fp_p: nstate=(fp_done)?bp_p:fp_p;
+        bp_p: nstate=(bp_done)?done_p:bp_p;
+        done_p: nstate=done_p;
+        default :nstate=init_p;
+    endcase
+end
+
+always @(posedge clk or negedge rstn) begin
+    if (!rstn)
+        state=init_p;
+    else
+        state=nstate;
+end
+
+//initialize
+wire sti_rd_init;
+wire [9:0] sti_addr_init;
+wire res_wr_init;
+wire [13:0]res_addr_init;
+wire [7:0]res_do_init;
+initialize init (clk, rstn, sti_rd_init, sti_addr_init, sti_di,
+                res_wr_init, res_addr_init, res_do_init, initialize_done);
+
+//forward pass
+wire res_wr_fp, res_rd_fp;
+wire [13:0] res_addr_fp;
+wire [7:0] res_do_fp;
+forward_pass fp (clk, rstn, initialize_done, res_rd_fp, res_wr_fp,
+                 res_addr_fp, res_do_fp, res_di, fp_done);
+
+//backward_pass
+wire res_wr_bp, res_rd_bp;
+wire [13:0] res_addr_bp;
+wire [7:0] res_do_bp;
+backward_pass bp (clk, rstn, fp_done, res_rd_bp, res_wr_bp,
+                 res_addr_bp, res_do_bp, res_di, bp_done);
+
+always @(*) begin
+    case (state)
+    init_p:begin
+        sti_rd=sti_rd_init;
+        sti_addr=sti_addr_init;
+        res_wr=res_wr_init;
+        res_rd=0;
+        res_addr=res_addr_init;
+        res_do=res_do_init;
+    end
+    fp_p:begin
+        sti_rd=0;
+        sti_addr=0;
+        res_wr=res_wr_fp;
+        res_rd=res_rd_fp;
+        res_addr=res_addr_fp;
+        res_do=res_do_fp;
+    end
+    bp_p:begin
+        sti_rd=0;
+        sti_addr=0;
+        res_wr=res_wr_bp;
+        res_rd=res_rd_bp;
+        res_addr=res_addr_bp;
+        res_do=res_do_bp;
+    end
+    done_p:begin
+        sti_rd=0;
+        sti_addr=0;
+        res_wr=0;
+        res_rd=0;
+        res_addr=0;
+        res_do=0;
+    end
+    default:begin
+        sti_rd=0;
+        sti_addr=0;
+        res_wr=0;
+        res_rd=0;
+        res_addr=0;
+        res_do=0;
+    end   
+    endcase
+end
+
+assign done=(state==done_p);
+
+endmodule
---
+++ B_ICC2017_preliminary_grad_cell-based/init.v
@@ -0,0 +1,121 @@
+module initialize (clk, rstn, sti_rd, sti_addr, sti_di, res_wr, res_addr, res_do, initialize_done);
+input clk, rstn;
+output sti_rd;
+output [9:0] sti_addr;
+input [15:0] sti_di;
+output res_wr;
+output [13:0]res_addr;
+output [7:0]res_do;
+output initialize_done;
+
+reg sti_rd_state, res_wr_state, initialize_done_state;
+parameter init=0, indicate_ROM=1, load_from_ROM=2, send_to_RAM_0=3, send_to_RAM=4, checkif_done=5, done=6;
+reg [2:0]state, nstate;
+reg [9:0]sti_addr_state;
+reg [3:0]buf_state, buf_nstate;
+reg [0:15]buff;
+reg [7:0]res_do_state;
+reg [13:0]res_addr_state;
+
+assign sti_addr=sti_addr_state;
+assign res_do=res_do_state;
+assign sti_addr=sti_addr_state;
+assign sti_rd=sti_rd_state;
+assign res_wr=res_wr_state;
+assign initialize_done=initialize_done_state;
+assign res_addr=res_addr_state;
+
+always @(*) 
+begin
+    case(state)
+        init: nstate=indicate_ROM;
+        indicate_ROM: nstate=load_from_ROM;
+        load_from_ROM: nstate=send_to_RAM_0;
+        send_to_RAM_0: nstate=send_to_RAM;
+        send_to_RAM: nstate=(buf_state==4'b1111)?checkif_done:send_to_RAM;
+        checkif_done: nstate=(sti_addr_state==1023)?done:indicate_ROM;
+        done: nstate=done;
+    endcase    
+end
+
+always @(posedge clk or negedge rstn) 
+begin
+    if(!rstn)
+        state=init;
+    else
+        state=nstate;    
+end
+
+always @(*) 
+begin
+    case(state)
+        init: begin
+            sti_rd_state=0;
+            res_wr_state=0;
+            initialize_done_state=0;
+            res_do_state=0;
+            buff=0;
+        end
+        indicate_ROM:begin
+            sti_rd_state=1;
+        end
+        load_from_ROM:begin
+            sti_rd_state=0;
+            buff=sti_di;
+        end
+        send_to_RAM_0:begin
+            res_do_state={7'b0, buff[buf_state]};
+            res_wr_state=1;
+        end
+        send_to_RAM:begin
+            res_do_state={7'b0, buff[buf_state]};
+            res_wr_state=1;
+        end
+        checkif_done:begin
+            res_wr_state=0;
+        end
+        done:begin
+            sti_rd_state=0;
+            res_wr_state=0;
+            initialize_done_state=1;
+        end
+
+    endcase
+end
+
+always @(*) begin
+    case (buf_state)
+        15: buf_nstate=15;
+        default :buf_nstate=buf_state+1;
+    endcase
+end
+
+always @(posedge clk) begin
+    case(state)
+        send_to_RAM: buf_state<=buf_nstate;
+        default: buf_state<=0;
+    endcase
+end
+
+always @(posedge clk)
+begin
+	case (state)
+	init:
+	begin
+		sti_addr_state=0;
+		res_addr_state=0;
+	end
+	send_to_RAM:
+		res_addr_state=res_addr_state+1;
+	checkif_done:
+	begin
+		if (sti_addr_state==1023)
+                	sti_addr_state=1023;
+            	else
+                	sti_addr_state=sti_addr_state+1;
+		res_addr_state=res_addr_state+1;
+	end
+	endcase
+end
+
+endmodule //init
